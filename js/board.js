128bfb5db981f7962f7b7447af5cf3865d1720ee
Andres Martinez <andresmartgonz@gmail.com>
2020-06-05 21:41:45 +0200

    Dynamic init game

--- js/board.js
+++ js/board.js
@@ -21,6 +21,27 @@ class Board {
     return emptyCard
   }
 
+  addBirdCard(arr) {
+    const birdCard = document.createElement("div")
+    birdCard.classList.add("col-2", arr[0].name)
+    birdCard.setAttribute("name", arr[0].name)
+
+    const badge = document.createElement("span")
+    badge.className = "badge"
+
+    birdCard.appendChild(badge)
+    arr.shift()
+
+    return birdCard
+  }
+
+  getRepeat(arr) {
+    return arr.reduce((acc, name) => {
+      acc[name] = (acc[name] || 0) + 1
+      return acc
+    }, {})
+  }
+
   getPlayersNames() {
     const url = new URL(window.location.href)
     this.playersNames.push(url.searchParams.get("player1"))
@@ -49,17 +70,35 @@ class Board {
 
       playerBlock.appendChild(rowCards)
 
-      for (i = 0; i < 8; i++) {
+      let birdsArr = []
+      for (let i = 0; i < 8; i++) {
+        birdsArr.push(this.cards[0].name)
+        this.cards.shift()
+      }
+
+      const birdObj = this.getRepeat(birdsArr)
+
+      Object.entries(birdObj).forEach(([key, value]) => {
         const birdCard = document.createElement("div")
-        birdCard.classList.add("col-2", this.cards[i].name)
-        birdCard.setAttribute("name", this.cards[i].name)
+        birdCard.classList.add("col-2", key)
+        birdCard.setAttribute("name", key)
 
         const badge = document.createElement("span")
         badge.className = "badge"
 
+        if (value > 1) {
+          badge.innerText = value
+        }
+
         birdCard.appendChild(badge)
-        this.cards.shift()
         rowCards.appendChild(birdCard)
+      })
+
+      const objLength = Object.keys(birdObj).length
+      if (!(objLength === 8)) {
+        for (let i = 0; i < 8 - objLength; i++) {
+          rowCards.appendChild(this.addEmptyCard())
+        }
       }
 
       this.remainDeck()
@@ -74,30 +113,20 @@ class Board {
       carouselItem.classList.add("carousel-item")
       const player = document.createElement("h4")
       if (el) {
-        player.innerText = `Colecciones de ${el}`
+        player.innerText = `Colección de ${el}`
       } else {
-        player.innerText = `Colecciones del jugador ${i}`
+        player.innerText = `Colección del jugador ${i}`
         i++
       }
 
       carouselInner.appendChild(carouselItem)
       carouselItem.appendChild(player)
 
-      const birdCard = document.createElement("div")
-      birdCard.classList.add("col-2", this.cards[0].name)
-      birdCard.setAttribute("name", this.cards[0].name)
-
-      const badge = document.createElement("span")
-      badge.className = "badge"
-
-      birdCard.appendChild(badge)
-      this.cards.shift()
-
       const rowCards = document.createElement("div")
       rowCards.classList.add("row", "cards")
 
       carouselItem.appendChild(rowCards)
-      rowCards.appendChild(birdCard)
+      rowCards.appendChild(this.addBirdCard(this.cards))
 
       for (let i = 0; i < 7; i++) {
         rowCards.appendChild(this.addEmptyCard())
@@ -116,18 +145,45 @@ class Board {
 
       gameRow.appendChild(this.addEmptyCard())
 
-      for (let j = 0; j < 3; j++) {
+      let birdsArr = []
+      for (let i = 0; i < 9; i++) {
+        birdsArr.push(cards[i].name)
+      }
+
+      const uniqueArr = [...new Set(birdsArr)].splice(0, 3)
+
+      for (let i = 0; i < 9; i++) {
+        if (uniqueArr[0] === cards[i].name) {
+          cards.splice(i, 1)
+          break
+        }
+      }
+
+      for (let i = 0; i < 9; i++) {
+        if (uniqueArr[1] === cards[i].name) {
+          cards.splice(i, 1)
+          break
+        }
+      }
+
+      for (let i = 0; i < 9; i++) {
+        if (uniqueArr[2] === cards[i].name) {
+          cards.splice(i, 1)
+          break
+        }
+      }
+
+      for (let i = 0; i < 3; i++) {
         const birdCard = document.createElement("div")
-        birdCard.classList.add("col-2", this.cards[j].name)
-        birdCard.setAttribute("name", this.cards[j].name)
+        birdCard.classList.add("col-2", uniqueArr[i])
+        birdCard.setAttribute("name", uniqueArr[i])
 
         const badge = document.createElement("span")
         badge.className = "badge"
 
         birdCard.appendChild(badge)
-        this.cards.shift()
-
         gameRow.appendChild(birdCard)
+
         boardContent.appendChild(gameRow)
       }
 
